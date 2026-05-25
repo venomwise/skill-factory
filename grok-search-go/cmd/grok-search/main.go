af17a59022d01eb9146668992196a8183e04f77e
venom <ch_venom@outlook.com>
2026-05-26 01:28:00 +0800

    fix(grok-search): 提交 CLI 入口文件

---
+++ grok-search-go/cmd/grok-search/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/venomwise/skill-factory/grok-search/cmd"
+)
+
+// Version information injected at build time via -ldflags.
+var (
+	version   = "dev"
+	commit    = "none"
+	date      = "unknown"
+	goVersion = "unknown"
+)
+
+func main() {
+	cmd.SetVersionInfo(version, commit, date, goVersion)
+
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
